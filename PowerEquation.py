8b04c529bd6e9eeca42ca96084d1a6b35fb4abbe
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2017-07-10 13:38:48 -0400

    clean up PowerEquation

--- PowerEquation.py
+++ PowerEquation.py
@@ -16,39 +16,43 @@ Let X be the number of such identities. Compute and return the value
 (X modulo (10^9 + 7)).
 """
 
-import copy
+def add_s(s, a, b, c, d, end):
+    """
+    Add power statements to the given set from the given numbers
+    """
+    forward  = set([(a, b * j, c, d * j) for j in xrange(1, end)])
+    backward = set([(c, d * j, a, b * j) for j in xrange(1, end)])
+    return s.union(forward).union(backward)
 
 def combos_from(n, pow_dict, last_n):
     """
     Returns the number of different power equations involving n and the
     numbers below n
     """
-    total = set([(1, n, 1, y) for y in xrange(1, n)] + [(1, y, 1, n) for y in xrange(1, n)])
+    # add the basics that are true for all n
+    total = set([(1, n, 1, y) for y in xrange(1, n)] +
+                [(1, y, 1, n) for y in xrange(1, n)])
     total = total.union(set([(n, y, n, y) for y in xrange(1, n)]))
     total = total.union(set([(y, n, y, n) for y in xrange(1, n + 1)]))
-    # 2n - 2 + n - 1 + n = 4n - 3
 
+    # add more interesting powers involving n
     for i in xrange(2, n):
         for power in xrange(2, last_n):
             i_to_power = pow(i, power)
-            if n == i_to_power:  # n is a power of something less than it (i)
-                if i not in pow_dict:
-                    pow_dict[i] = set()
+            if n == i_to_power:
+                if i not in pow_dict: pow_dict[i] = set()
                 for p, num in pow_dict[i]:
-                    total = total.union(set([(n, p * j, num, power * j) for j in xrange(1, last_n / power + 1)]))
-                    total = total.union(set([(num, power * j, n, p * j) for j in xrange(1, last_n / power + 1)]))
+                    total = add_s(total, n, p, num, power, last_n / power + 1)
                 pow_dict[i].add((power, n))
-                total = total.union(set([(n, j, i, power * j) for j in xrange(1, last_n / power + 1)]))
-                total = total.union(set([(i, power * j, n, j) for j in xrange(1, last_n / power + 1)]))
+                total = add_s(total, n, 1, i, power, last_n / power + 1)
                 break
             elif n < i_to_power:
                 break
 
     # show that each item in the set is indeed correct
     for a, b, c, d in total:
-        assert (a <= last_n and b <= last_n and c <= last_n and d <= last_n)
-        assert (a >= 0 and b >= 0 and c >= 0 and d >= 0)
-        assert (pow(a, b) == pow(c, d))
+        assert a <= last_n and b <= last_n and c <= last_n and d <= last_n and\
+            a >= 0 and b >= 0 and c >= 0 and d >= 0 and pow(a, b) == pow(c, d)
     return len(total)
 
 def count(n):
