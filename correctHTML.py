40c090baf5402b3df734968e3fb8ae36e307a189
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2017-09-05 23:07:39 -0400

    fixed correctHTML to pass more rigorous testing

--- correctHTML.py
+++ correctHTML.py
@@ -19,14 +19,39 @@ And this is an example of incorrectly nested tags:
 
 import re
 
+def check_tags(l):
+    """
+    Given line of HTML, checks that HTML tags match. If not, prints the line.
+    """
+    # start on outside working inwords, checking first and last tags match
+    stack = []
+    for item in l:
+        if   item[-2] == '/' or item[1] == '!':
+           continue # deal with self-closing tags and comments
+        elif item[ 1] == '/':
+           root  = item[2 : -1].split()[0]
+           try:
+                match = stack.pop()
+           except: return False # closing tag without an opener
+           if root != match:
+               return False
+        else:
+           root = item[1 : -1].split()[0]
+           stack.append(root) 
+    return True
+
 def correct(filename):
     """
     Executes the solution to the problem described in the problem statement
     """
     with open(filename) as f:
-         html = f.read()
-    l = re.findall(r'</\w+>|<\w+>', html)
-    for i in xrange(len(l)/2):
-        if l[len(l) - i - 1] != l[i][0] + '/' + l[i][1:]:
-            return False
-    return True
+        html  = f.read()
+        lines = html.split('\n')
+
+    for line in lines:
+        l = re.findall(r'<[^>]+>', line)
+        if len(l) == 1: continue # deal with lines containing only 1 tag
+        correct = check_tags(l)
+        if not correct:
+            print "Incorrect:", line
+
